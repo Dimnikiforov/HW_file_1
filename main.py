cfea46537c7960b352faef2fd36002a5fb44480a
Dima Nikiforov <dim.nikiforov@gmail.com>
2022-12-05 14:51:08 +0300

    Initial commit Выполняется чтение из файла

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-'''задача №1'''
+with open('recipes.txt', encoding='utf-8') as file:
+    cook_book = {}
+    for line in file:
+        dish = line.strip()
+        ingredients_count = int(file.readline())
+        ingredients = []
+        for i in range(ingredients_count):
+            ingredient = file.readline().strip()
+            ingredient_name, quantity, measure = ingredient.split(' | ')
+            ingredients.append({
+                'ingredient_name': ingredient_name,
+                'quantity': quantity,
+                'measure': measure
+            })
+        file.readline()
+        cook_book[dish] = ingredients
+from pprint import pprint
+pprint(cook_book)
