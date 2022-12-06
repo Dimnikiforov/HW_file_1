2d8ad446ea1d35048b5f5aef88a0e938fab3d9d1
Dima Nikiforov <dim.nikiforov@gmail.com>
2022-12-06 13:56:37 +0300

    Initial commit Выполняется чтение из файла Добавление функции выбора блюд в меню Добавление функции составления меню

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ with open('recipes.txt', encoding='utf-8') as file:
         cook_book[dish] = ingredients
 from pprint import pprint
 pprint(cook_book)
+def get_shop_list_by_dishes(dishes, person):
+    total = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for j in cook_book[dish]:
+                if j['ingredient_name'] not in total:
+                    total[j['ingredient_name']] = {'measure': j['measure'],
+                                                   'quantity': (int(j[('quantity')]) * person)}
+                else:
+                    total[j['ingredient_name']]['quantity'] += int(j['quantity']) * person
+    return total
+print(get_shop_list_by_dishes(['Фахитос', 'Омлет'], 1))
+
